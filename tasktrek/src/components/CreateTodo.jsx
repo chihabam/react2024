cd8d4ba10b7e76aeb3cab7da5e679eefe09359fe
Magus Chihaba <Magus Samuel Chihaba>
2024-04-10 23:44:10 -0500

    Added the css stuff

--- tasktrek/src/components/CreateTodo.jsx
+++ tasktrek/src/components/CreateTodo.jsx
@@ -1,12 +1,21 @@
-import React from "react";
+import React, {useState} from "react";
 
-function CreateTodo(){
-    let tasks =0;
+const CreateTodo=()=>{
+    const [count,setCount]= useState(0);
+    const [input,setInput]=useState("");
+    const tasks = ["Task 1","Task 2","Task 3"]
+    console.log(count);
+
     const hideButton = true;
+
     const handleClick=()=>{
-        tasks++;
-        console.log("Add Task")
+        setCount(count+1);
+    };
+
+    const handleChange=()=>{
+        setInput(event.target.value)
     }
+
     const countTasks =()=>{
        return tasks>0? countTasks:"No tasks available";
     
@@ -14,8 +23,15 @@ function CreateTodo(){
 
     return (
     <>
-        <h1>{countTasks()}</h1>
+        <h1>Tasks: {count}</h1>
+        <input type="text" onChange={handleChange}/>
         <button onClick={handleClick} className='custo-btn' disabled={!hideButton}>Add Task</button>
+        <h1>Input: {input}</h1>
+        <ul>
+            {
+                tasks.map(task=><li key={task}>{task}</li>)
+            }
+        </ul>
     </>);
 }
 
