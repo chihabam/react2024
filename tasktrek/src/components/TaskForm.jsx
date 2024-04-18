dfffc8a84eb6e59d3c976a8e25ad8ab4a8107a48
Magus Chihaba <Magus Samuel Chihaba>
2024-04-17 21:39:49 -0500

    new changes

--- tasktrek/src/components/TaskForm.jsx
+++ tasktrek/src/components/TaskForm.jsx
@@ -9,16 +9,19 @@ const TaskForm = () => {
             placeholder='Enter your task'/>
 
             <div className='task_form_bottom_line'>
+              <div>
                 <button className='tag'>HTML</button>
                 <button className='tag'>CSS</button>
                 <button className='tag'>Javascript</button>
                 <button className='tag'>React</button>
-
+                </div>
+                <div>
                 <select className='task_status'>
                     <option value='todo'>To do</option>
                     <option value='doing'>Doing</option>
                     <option value='done'>Done</option>
                 </select>
+                </div>
                 <button type="submit" className='task_submit'>+ Add Task</button>
             </div>
         </form>
