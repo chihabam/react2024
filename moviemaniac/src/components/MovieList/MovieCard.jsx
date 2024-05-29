8730faa4c0216635de9403843c844f68f1c30755
Magus Chihaba <Magus Samuel Chihaba>
2024-05-28 22:59:02 -0500

    Updated movie list

--- moviemaniac/src/components/MovieList/MovieCard.jsx
+++ moviemaniac/src/components/MovieList/MovieCard.jsx
@@ -14,7 +14,7 @@ const MovieCard = ({movie}) => {
             <h3 className="movie_details_heading">{movie.original_title}</h3>
             <div className="align_center movie_date_rate">
                 <p> {movie.release_date}</p>
-                <p>{movie.rate_average}<img src={Star} alt="" className="card_emoji" /></p>
+                <p>{movie.vote_average}<img src={Star} alt="" className="card_emoji" /></p>
             </div>
             <p className="movie_description">{movie.overview.slice(0,100) + "..."}</p>
         </div>
--- moviemaniac/src/components/MovieList/MovieList.jsx
+++ moviemaniac/src/components/MovieList/MovieList.jsx
@@ -2,21 +2,48 @@ import React, { useEffect,useState } from 'react'
 import './MovieList.css'
 import Fire from '../../assets/fire.png'
 import MovieCard from './MovieCard'
+import FilterGroup from './FilterGroup'
 
 const MovieList = () => {
-    const [movies, setmovies] = useState([])
+    const [movies, setMovies] = useState([])
+    const [filterMovies,setFilterMovies] =useState([])
+    const [minRating,setMinRating] = useState(0)
+    const [sort,setSort]= useState({
+        by:"default",
+        order:"asc"
+    });
     useEffect(()=>{
         fetchMovies();
     }, []);
+
+    const handleSort=(e)=>{
+        const {name,value} = e.target;
+        setSort(prev=>{
+            return{...prev,[name]:value}
+        })
+    }
+    console.log(sort)
 
     const fetchMovies =async()=>{
         const response = await fetch("https://api.themoviedb.org/3/movie/popular?api_key=908aab0fb2990bbe9f5724bc3c3a91a0")
         // .then((res)=>res.json())
         // .then((data)=>console.log(data));
        const data = await response.json();
-       setmovies(data.results)
+       setMovies(data.results)
+       setFilterMovies(data.results)
        console.log(data)
        return data;
+    }
+
+    const handleFilter = rate =>{
+        if(rate === minRating){
+            setMinRating(0)
+            setFilterMovies(movies)
+        }else{
+            setMinRating(rate);
+            const filtered = movies.filter(movie=> movie.vote_average >=rate);
+            setFilterMovies(filtered);
+        }
     }
 
   return (
@@ -27,27 +54,23 @@ const MovieList = () => {
                 
             </h2>
             <div className='align_center movie_list_fs'>
-                <ul className="align_center movie_filter">
-                    <li className="movie_filter_item active">8+ Star</li>
-                    <li className="movie_filter_item">7+ Star</li>
-                    <li className="movie_filter_item">6+ Star</li>
-                </ul>
+                <FilterGroup minRating= {minRating} onRatingClick={handleFilter} ratings={[8,7,6,5,4]}/>
 
-                <select name="" id="" className="movie_sorting">
-                    <option value="">Sort By</option>
-                    <option value="">Date</option>
-                    <option value="">Rating</option>
+                <select name="by" id="" className="movie_sorting" onChange={handleSort} value ={sort.by}>
+                    <option value="default">Sort By</option>
+                    <option value="release_date">Date</option>
+                    <option value="vote_average">Rating</option>
                 </select>
 
-                <select name="" id="" className="movie_sorting">
-                    <option value="">Ascending</option>
-                    <option value="">Descending</option>
+                <select name="order" id="" className="movie_sorting" onChange={handleSort} value ={sort.order}>
+                    <option value="asc">Ascending</option>
+                    <option value="desc">Descending</option>
                 </select>
             </div>
         </header>
         <div className="movie_cards">
             {
-                movies.map(movie=> <MovieCard key= {movie.id}  movie ={movie}/>)
+                filterMovies.map(movie=> <MovieCard key= {movie.id}  movie ={movie}/>)
             }
         </div>
     </section>
